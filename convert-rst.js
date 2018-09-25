e25a262645a38fd583752e08abe987649f48adf9
Ruslan Kazakov <ruslan.kazakov@gmail.com>
2018-09-25 12:28:33 +1000

    Add RST

---
+++ convert-rst.js
@@ -0,0 +1,28 @@
+var TRANSLATION = 'RST';
+var USFM_FILE = 'usfm/' + TRANSLATION.toLowerCase() + '.usfm';
+var JSON_FILE = 'json/' + TRANSLATION.toLowerCase() + '.json';
+var HEADERS = ['book', 'chapter', 'verse', 'text'];
+
+var bookRefs = require('./lib/en/bookRefs').bookRefs;
+var bookNames = require('./lib/en/bookNames').bookNames;
+
+var fs = require('fs');
+var Converter = require("csvtojson").Converter;
+var converter = new Converter({
+  delimiter: '\t',
+  constructResult: false,
+  headers: HEADERS
+});
+
+converter.transform = function(json, row, index) {
+  // adding columns
+  json['tran'] = TRANSLATION;
+  json['bookRef'] = bookRefs[json['book']];
+  json['bookName'] = bookNames[json['book']];
+  json['book'] = Number(json['book'].slice(0, -1));
+};
+
+var readStream = fs.createReadStream(USFM_FILE);
+var writeStream = fs.createWriteStream(JSON_FILE);
+
+readStream.pipe(converter).pipe(writeStream);
